574d00d266c3f48c31ccd1644fa56ab4f2b20e90
Harjit <harjitk89@hotmail.co.uk>
2018-08-21 15:37:13 +0100

    about me styling added

--- bbc_history_app/src/components/About.js
+++ bbc_history_app/src/components/About.js
@@ -8,7 +8,12 @@ const About = () => (
   </div>
 <div className="about-us">
   <h1>About Us</h1>
-  <p>The brief was...</p>
+  <p>BBC Walk In History is an educational app built using JavaScript and React for anyone
+    wishing to improve their history knowledge!
+    This application allows the user to select a particular date from the calender and
+    these events are displayed in an interactive
+    timeline. The user is also able to filter events by births and deaths.
+  </p>
 </div>
 <div className="headImage">
   <img src="landing-logo.png" className = "headImage" alt="Landing Logo"/>
